a938e1e11654a5d199ecff59a135e3bed2435521
unknown <charles_barrett24@mymail.eku.edu>
2020-10-28 16:50:07 -0400

    Removed cyprus cypress tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import TicTacToe from "./components/TicTacToe";
-import "./commands";
+/*import "./commands";
 import "cypress-axe";
 import "@testing-library/cypress/add-commands";
-
+*/
 class Home extends React.Component{
 	render(){
 		return(<div> <TicTacToe/> </div>);
