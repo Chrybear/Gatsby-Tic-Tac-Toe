b685f6c0f79c2d8e0b62a18e586ba65584074490
unknown <charles_barrett24@mymail.eku.edu>
2020-10-28 17:18:34 -0400

    Added penalty for stalemates

--- src/pages/components/game.js
+++ src/pages/components/game.js
@@ -2,7 +2,8 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import './index.css';
 import Board from './board.js';
-import DK from "./Donkey_Kong2.jpg"
+import DK from "./Donkey_Kong2.jpg";
+import DK_sad from "./DK_dissapointed.gif";
 
 class Game extends React.Component {
       constructor(props) {
@@ -87,21 +88,40 @@ class Game extends React.Component {
 		
 		if (status.substring(0, 6) === "Winner"){
 			return (
-          <div className="game">
-            <div className="game-board">
-              <Board
-                squares={current.squares}
-                onClick={i => this.handleClick(i)}
-              />
-            </div>
-            <div className="game-info">
-              <div>{status}</div>
-              <ol>{moves}</ol>
-              </div>
-			  <br/>
-			  <img src ={DK} alt="Oh, Banana"/>
-          </div>
-        );
+			  <div className="game">
+				<div className="game-board">
+				  <Board
+					squares={current.squares}
+					onClick={i => this.handleClick(i)}
+				  />
+				</div>
+				<div className="game-info">
+				  <div>{status}</div>
+				  <ol>{moves}</ol>
+				  </div>
+				  <br/>
+				  <img src ={DK} alt="Oh, Banana"/>
+			  </div>
+			);
+		}else if (this.state.stepNumber >= 9){
+			status = "STALEMATE";
+			return (
+			  <div className="game">
+				<div className="game-board">
+				  <Board
+					squares={current.squares}
+					onClick={i => this.handleClick(i)}
+				  />
+				</div>
+				<div className="game-info">
+				  <div>{status}</div>
+				  <div>Return to the past!</div>
+				  <ol>{moves}</ol>
+				  </div>
+				  <br/>
+				  <img src ={DK_sad} alt="Oh, Banana..."/>
+				</div>
+			);
 		}else{
 			return (
 			  <div className="game">
